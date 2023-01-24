03d4eeab4600586293b37b22f226c2e0ecc2747f
SaikrishnaPanda <56650585+SaikrishnaPanda@users.noreply.github.com>
2023-01-24 20:57:43 +0530

    ReaderController updated

--- src/main/java/com/library/main/controller/ReaderController.java
+++ src/main/java/com/library/main/controller/ReaderController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +45,17 @@ public class ReaderController {
 		
 		Reader reader = optional.get();
 		return ResponseEntity.status(HttpStatus.OK).body(reader);
+	}
+	@PutMapping("/update/{rid}")
+	public ResponseEntity<String> updateReader(@RequestBody Reader readertoupdate, @PathVariable("rid") int rid ) {
+		Reader reader = (Reader)readerService.getReaderById(rid).get();
+		reader.setName(readertoupdate.getName());
+		readerService.updateReader(reader);
+		return ResponseEntity.status(HttpStatus.OK).body("Reader updated");
+	}
+	@DeleteMapping("/delete/{rid}")
+	public ResponseEntity<String> deleteReader(@PathVariable("rid")int rid) {
+		readerService.deleteReader(rid);
+		return ResponseEntity.status(HttpStatus.OK).body("Reader deleted");
 	}
 }
