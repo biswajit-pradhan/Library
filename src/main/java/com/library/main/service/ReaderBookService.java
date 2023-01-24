d28e4995d074b80b56a6fb04bd668faf99a8f801
SaikrishnaPanda <56650585+SaikrishnaPanda@users.noreply.github.com>
2023-01-24 22:59:13 +0530

    getReaderByBookId API created on ReaderBookController

--- src/main/java/com/library/main/service/ReaderBookService.java
+++ src/main/java/com/library/main/service/ReaderBookService.java
@@ -1,9 +1,13 @@
 package com.library.main.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.library.main.data.ReaderBookRepository;
+import com.library.main.model.Reader;
 import com.library.main.model.ReaderBook;
 @Service
 public class ReaderBookService {
@@ -15,5 +19,15 @@ public class ReaderBookService {
 		
 		readerBookRepository.save(readerBook);
 	}
+
+	public List<ReaderBook> getReadersByBookId(int bid) {
+		List<ReaderBook> list = readerBookRepository.findAll();
+		List<ReaderBook> filteredlist =	list.stream()
+											.filter(e->e.getBook().getId()==bid)
+											.collect(Collectors.toList());
+		return filteredlist;
+	}
+
+
 
 }
