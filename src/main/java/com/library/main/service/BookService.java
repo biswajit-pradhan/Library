57721a8f5b566129e025a0854b9758d14fceaa90
SaikrishnaPanda <56650585+SaikrishnaPanda@users.noreply.github.com>
2023-01-24 16:05:42 +0530

    ReaderController and assigned book tp reader added

--- src/main/java/com/library/main/service/BookService.java
+++ src/main/java/com/library/main/service/BookService.java
@@ -1,5 +1,20 @@
 package com.library.main.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.library.main.data.BookRepository;
+import com.library.main.model.Book;
+@Service
 public class BookService {
+	@Autowired
+	private BookRepository bookRepository;
+
+	public Optional<Book> getBookByID(int bookId) {
+		Optional<Book> optional = bookRepository.findById(bookId);
+		return optional;
+	}
 
 }
--- src/main/java/com/library/main/service/ReaderBookService.java
+++ src/main/java/com/library/main/service/ReaderBookService.java
@@ -1,5 +1,17 @@
 package com.library.main.service;
 
+import org.springframework.stereotype.Service;
+
+import com.library.main.data.ReaderBookRepository;
+import com.library.main.model.ReaderBook;
+@Service
 public class ReaderBookService {
+	
+	private ReaderBookRepository readerBookRepository;
+
+	public void assign(ReaderBook readerBook) {
+		
+		readerBookRepository.save(readerBook);
+	}
 
 }
