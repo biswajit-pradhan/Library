3e5550de32b71b26d8f3109f47a204e086f3478d
Dilip Kumar <122096399+dilip973@users.noreply.github.com>
2023-01-25 13:11:18 +0530

    Data updated

--- src/main/java/com/library/main/service/PublisherService.java
+++ src/main/java/com/library/main/service/PublisherService.java
@@ -43,5 +43,45 @@ public class PublisherService {
 		
 	}
 
+	public Optional<Double> calculateTotalRent(int week) {
+		double totalRent = 0;
+		if(week == 1) {
+			totalRent = getPrice() * 0.1;
+		}else if(week == 2) {
+			totalRent =getPrice() * 0.15;
+		}
+		else if(week == 3) {
+			totalRent =getPrice() * 0.20;
+		}
+		else if(week == 4) {
+			totalRent =getPrice() * 0.25;
+		}
+		
+		else {
+			return Optional.empty();
+		}
+		return Optional.of(totalRent) ;
+	}
+
+	private double getPrice() {
+		
+		return 0;
+	}
+
+	public Object getTotalRentByPublisherId(double bookprice, int totalDays) {
+
+			if (totalDays <= 7 && totalDays > 0) {
+				return (bookprice / 100) * 10;
+			} else if (totalDays > 7 && totalDays <= 14) {
+				return (bookprice / 100) * 15;
+			} else if (totalDays > 14 && totalDays <= 21) {
+				return (bookprice / 100) * 20;
+			} else if (totalDays > 21 && totalDays <= 30) {
+				return (bookprice / 100) * 25;
+			} else {
+				return "You can take book rent for maximum 30 days only..";
+			}
+		}
+
 	
 }
