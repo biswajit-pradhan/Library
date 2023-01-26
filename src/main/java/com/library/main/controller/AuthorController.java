407c808cb31a4580d4f706eab6a1945b57fd3e87
Biswajit Pradhan <pbiswajit967@gmail.com>
2023-01-26 16:19:11 +0530

    Conflict Solved

--- src/main/java/com/library/main/controller/AuthorController.java
+++ src/main/java/com/library/main/controller/AuthorController.java
@@ -18,11 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.library.main.model.Author;
 import com.library.main.model.Book;
-<<<<<<< HEAD
-import com.library.main.model.Publisher;
-import com.library.main.model.Reader;
-=======
->>>>>>> 8b8202e0f4027281468ad5fd3fefdd305e8c4bca
 import com.library.main.service.AuthorService;
 import com.library.main.service.BookService;
 
@@ -77,8 +72,6 @@ public class AuthorController {
 		return ResponseEntity.status(HttpStatus.OK).body("author deleted from database");
 	}
 
-<<<<<<< HEAD
-=======
 	@GetMapping("/gettotalrentbyauthorid/{aid}")
 	public ResponseEntity<Object> getTotalRentByAuthorId(@PathVariable("aid") int aid) {
 		Optional<Author> optional = authorService.getAuthorById(aid);
@@ -89,7 +82,5 @@ public class AuthorController {
 		Object totalCost=authorService.getTotalRentByAuthorId(book);
 		return ResponseEntity.status(HttpStatus.OK).body(totalCost);
 	}
-
->>>>>>> 8b8202e0f4027281468ad5fd3fefdd305e8c4bca
 
 }
